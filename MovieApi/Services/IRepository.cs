e42a7dd8382f452b48c32b9ee12430728923b009
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-09-22 14:48:09 -0400

    Finally...
    Got the stupid GIT to work.

---
+++ MovieApi/Services/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Services
+{
+    public interface IRepository
+    {
+        Task<List<Genre>> GetAllGenres();
+        Genre GetGenreById(int Id);
+    }
+}
---
+++ MovieApi/Services/InMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using MovieApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Services
+{
+    public class InMemoryRepository : IRepository
+    {
+        private List<Genre> _genres;
+
+        public InMemoryRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre(){Id= 1, Name="Comedy"},
+                new Genre(){Id= 2, Name="Action"}
+            };
+        }
+
+        public async Task<List<Genre>> GetAllGenres()
+        {
+            await Task.Delay(1500);
+            return _genres;
+        }
+
+        public Genre GetGenreById(int Id)
+        {
+            return _genres.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
