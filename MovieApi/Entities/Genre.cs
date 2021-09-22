07180f012724ad0ebff5d6f19e8c02608c63185e
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-09-22 16:38:46 -0400

    Sync

--- MovieApi/Entities/Genre.cs
+++ MovieApi/Entities/Genre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieApi.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Entities
 {
-    public class Genre
+    public class Genre : IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage = "The field with name {0} is required")]
         [StringLength(50)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18,120)]
@@ -22,5 +24,18 @@ namespace MovieApi.Entities
 
         [Url]
         public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be uppercase", new string[] {nameof(Name)});
+                }
+            }
+        }
     }
 }
